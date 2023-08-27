12fd6f793ed30d8f91416e5468ccf5d10400f12c
Jakub Buda <jbuda@eshopworld.com>
2023-08-27 20:28:26 +0200

    Move interrupt handler into ecat application file

--- examples/SOES_CIA402_AX58100/src/ecatapp.c
+++ examples/SOES_CIA402_AX58100/src/ecatapp.c
@@ -25,30 +25,54 @@ void app_cia402_mc(void);
 
 /* SOES configuration */
 static esc_cfg_t config = { 
-    .user_arg = "ax58100",
-    .use_interrupt = 1,
-    .watchdog_cnt = INT32_MAX,
-    .set_defaults_hook = NULL,
-    .pre_state_change_hook = NULL,
-    .post_state_change_hook = NULL,
-    .application_hook = ecatapp,
-    .safeoutput_override = NULL,
-    .pre_object_download_hook = NULL,
+    .user_arg                  = "ax58100",
+    .use_interrupt             = 1,
+    .watchdog_cnt              = INT32_MAX,
+    .set_defaults_hook         = NULL,
+    .pre_state_change_hook     = NULL,
+    .post_state_change_hook    = NULL,
+    .application_hook          = ecatapp,
+    .safeoutput_override       = NULL,
+    .pre_object_download_hook  = NULL,
     .post_object_download_hook = NULL,
-    .rxpdo_override = rxpdo_override,
-    .txpdo_override = txpdo_override,
-    .esc_hw_interrupt_enable = ESC_interrupt_enable,
-    .esc_hw_interrupt_disable = ESC_interrupt_disable,
-    .esc_hw_eep_handler = NULL,
-    .esc_check_dc_handler = check_dc_handler,
+    .rxpdo_override            = rxpdo_override,
+    .txpdo_override            = txpdo_override,
+    .esc_hw_interrupt_enable   = ESC_interrupt_enable,
+    .esc_hw_interrupt_disable  = ESC_interrupt_disable,
+    .esc_hw_eep_handler        = NULL,
+    .esc_check_dc_handler      = check_dc_handler,
 };
 
 /* CiA402 motion control configuration */
 cia402_axis_t cia402axis = {
-    .init_od_hook = app_cia402_init,
-    .motion_control_hook = app_cia402_mc,
+    .init_od_hook              = app_cia402_init,
+    .motion_control_hook       = app_cia402_mc,
 };
 
+// **************************************************************
+
+static uint8_t sync0_irq_flag = 0;
+
+void EXTI1_IRQHandler(void)
+{
+    if(EXTI_GetITStatus(EXTI_Line1) != RESET)
+    {
+        EXTI_ClearITPendingBit(EXTI_Line1);
+        sync0_irq_flag = 1;
+    }
+}
+
+static uint8_t pdi_irq_flag = 0;
+
+void EXTI3_IRQHandler(void)
+{
+    if(EXTI_GetITStatus(EXTI_Line3) != RESET)
+    {
+        EXTI_ClearITPendingBit(EXTI_Line3);
+        pdi_irq_flag = 1;
+    }
+}
+// **************************************************************
 
 void ecatapp_init(void) {
     ecat_slv_init(&config);
@@ -113,3 +137,26 @@ void app_cia402_mc()
     *(cia402axis.statusword) |= CIA402_STATUSWORD_CSP_DRIVE_FOLLOWS_COMMAND;
 }
  
+void ecatapp_loop(void)
+{
+    // stack in mixed mode
+    if (sync0_irq_flag) {
+        ESC_updateALevent();        
+        DIG_process (DIG_PROCESS_APP_HOOK_FLAG | DIG_PROCESS_INPUTS_FLAG);
+        sync0_irq_flag = 0;
+    }
+    if (pdi_irq_flag) {
+        ESC_updateALevent();
+        if (ESCvar.dcsync) {
+            DIG_process (DIG_PROCESS_OUTPUTS_FLAG);    
+        } else {
+            DIG_process (DIG_PROCESS_OUTPUTS_FLAG | DIG_PROCESS_APP_HOOK_FLAG | DIG_PROCESS_INPUTS_FLAG);
+        }
+        pdi_irq_flag = 0;
+    } else {
+        // ecat_slv_worker(ESCREG_ALEVENT_CONTROL | ESCREG_ALEVENT_SMCHANGE
+        //                 | ESCREG_ALEVENT_SM0 | ESCREG_ALEVENT_SM1);
+        ecat_slv_poll();
+        DIG_process(DIG_PROCESS_WD_FLAG);
+    }
+}
